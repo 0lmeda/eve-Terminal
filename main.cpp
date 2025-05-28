265dbc05534184130ee6e67d1e5ed69a3ac7fd8a
Daniel O. <151310780+0lmeda@users.noreply.github.com>
2025-05-28 07:02:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,20 +9,30 @@ using namespace std;
 
 
 int main (){
+    //variables
     char cwd[1024];
     string input;
+
+    //Loop for the terminal
     while(true){
-   
+
+
+        //Get current directory
         getcwd(cwd, sizeof(cwd));
+
+        //show current directory
         cout<<"    $ ("<<cwd<<")\n";
         cout<<"eve >>";
 
+        //get the command from the user
         getline(cin, input);
 
 
         if(input == "exit"){
             break;
         }
+
+        //if the user doesn't enter anything, repeat the loop
         if(input.empty()){
 
             continue;
@@ -44,23 +54,28 @@ int main (){
         }
 
         
-
+    //if the word still have something, save it in the vector
         if(!word.empty()){
             arguments.push_back(word);
         }
 
-
+        //get the command, which will be the first word of what user entered
         string command = arguments[0];
 
+
+        //Manage manually cd
         if(command == "cd"){
+            //variable to save the path
             const char* path;
+            //if there are something after cd, it will be the path, if it doesn't, it will get a defaul value ("HOME")
             if(arguments.size()>1){
                 path = arguments[1].c_str();
             }
+                
             else{
                 path = getenv("HOME");
             }
-
+            //if cd gets a problem
             if(chdir(path)!=0){
                 perror("CD failed");
             }
@@ -68,12 +83,14 @@ int main (){
 
         }
 
+        //Transform the vector into the char* vectors
         vector<char*> args;
 
         for(auto& p : arguments){
             args.push_back(&p[0]);
         }
 
+        //Add nullptr at the end of the vector, because if it doesn't it will be a infinite loop
         args.push_back(nullptr);
 
 
